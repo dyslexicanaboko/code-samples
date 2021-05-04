d964efa70f5422226c22acf4fdc2ab58d4b629de
Volta\Eli <dyslexicanaboko@gmail.com>
2021-05-03 22:01:04 -0400

    Initial push. Moving the dom-examples code from the incubator to this repo where I will put code examples.

---
+++ c/add-numbers.c
@@ -0,0 +1,16 @@
+int AddNumbers(int x, int y);
+
+//Press F6 to compile
+int main()
+{
+    int z = AddNumbers(1, 6);
+
+    printf("Blah: %i\n", z);
+}
+
+int AddNumbers(int x, int y)
+{
+    int z = x + y;
+
+    return z;
+}
