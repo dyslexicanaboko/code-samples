329c06112cdc7d332090b16f1b459e6cd879ac24
Volta\Eli <dyslexicanaboko@gmail.com>
2022-07-10 02:29:56 -0400

    Added two functions now. One for just straight concatenation and the other for returning a string pointer. It's semi-working. Still need to work out some kinks.

--- c/string-operations.c
+++ c/string-operations.c
@@ -1,9 +1,27 @@
 //http://www.cplusplus.com/reference/cstring/
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+void StringConcatenationNoReturn();
+char *StringConcatenationWithReturn();
 
 //Press F6 to compile
 int main()
+{
+    StringConcatenationNoReturn();
+    
+    char *str = StringConcatenationWithReturn();
+
+    printf("Returned concatenated string: %s", str);
+
+    free(str);
+
+    return 0;
+}
+
+//Concatenating a string is easy if you don't plan on returning it from a function
+void StringConcatenationNoReturn()
 {
     //You need to specify the string length you will need, anything shorter and there will be a malfunction
     char str[8] = "Alpha";
@@ -11,7 +29,23 @@ int main()
     //The second parameter is appended to the end of the first parameter and stored in the first parameter
     strcat(str, "bet");
 
-    printf("Concatenated string: %s", str);
+    printf("Concatenated string: %s\n", str);
+}
 
-    return 0;
+//It gets more complicated when you need to return the string
+//https://stackoverflow.com/a/25799033/603807
+char *StringConcatenationWithReturn()
+{
+    char str[9] = "Alpha";
+
+    strcat(str, "bet2");
+
+    //printf("String length: %i\n", strlen(str));
+
+    //Allocating space on the heap for the string so it can be returned as a pointer
+    char *strReturn = (char*)malloc((strlen(str) + 1)*sizeof(char));
+    
+    strReturn = &str;
+
+    return strReturn;
 }
